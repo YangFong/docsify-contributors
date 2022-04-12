4527132050d858a97c4ebac0974ae8ec0fadee67
YangQi <yangfong2022@qq.com>
2022-04-12 16:26:52 +0800

    perf: code compression (#2)

---
+++ esbuild.config.js
@@ -0,0 +1,22 @@
+const { build } = require('esbuild');
+const fs = require('fs/promises');
+
+const compressPlugin = {
+    name: 'compress',
+    setup(build) {
+        build.onEnd(async () => {
+            const path = __dirname + '/dist/index.min.js';
+            const res = await fs.readFile(path);
+            const data = res.toString();
+            await fs.writeFile(path, data.replace(/\s{2,}/g, ''));
+        });
+    }
+};
+
+build({
+    entryPoints: ['./src/core/index.ts'],
+    bundle: true,
+    minify: true,
+    outfile: '/dist/index.min.js',
+    plugins: [compressPlugin]
+}).catch(() => process.exit(1));
